49a3e4ba66460a4ab27e1b4e80c295744f87903a
Shreyansh2610 <shreyansh7874056628@gmail.com>
2024-07-24 23:54:41 +0530

    revert api routes

--- routes/api.php
+++ routes/api.php
@@ -33,34 +33,34 @@ Route::middleware(['auth:api'])->group(function () {
 
     Route::put('/company-profile', [CompanyProfileController::class,'update']);
 
-    // Route::apiResource('banks',BankController::class)->except(['destroy','show','update']);
-    Route::get('/banks-get',[BankController::class,'index']);
-    Route::post('/banks',[BankController::class,'store']);
+    Route::apiResource('banks',BankController::class)->except(['destroy','show','update']);
+    // Route::get('/banks-get',[BankController::class,'index']);
+    // Route::post('/banks',[BankController::class,'store']);
 
-    // Route::apiResource('products',ProductsController::class)->except(['show']);
-    Route::get('/products-get',[ProductsController::class,'index']);
-    Route::post('/products',[ProductsController::class,'store']);
-    Route::put('/products/{id}',[ProductsController::class,'update']);
-    Route::delete('/products/{id}',[ProductsController::class,'destroy']);
+    Route::apiResource('products',ProductsController::class)->except(['show']);
+    // Route::get('/products-get',[ProductsController::class,'index']);
+    // Route::post('/products',[ProductsController::class,'store']);
+    // Route::put('/products/{id}',[ProductsController::class,'update']);
+    // Route::delete('/products/{id}',[ProductsController::class,'destroy']);
 
 
-    // Route::apiResource('sizes',SizeController::class)->except(['show']);
-    Route::get('/sizes-get',[SizeController::class,'index']);
-    Route::post('/sizes',[SizeController::class,'store']);
-    Route::put('/sizes/{id}',[SizeController::class,'update']);
-    Route::delete('/sizes/{id}',[SizeController::class,'destroy']);
+    Route::apiResource('sizes',SizeController::class)->except(['show']);
+    // Route::get('/sizes-get',[SizeController::class,'index']);
+    // Route::post('/sizes',[SizeController::class,'store']);
+    // Route::put('/sizes/{id}',[SizeController::class,'update']);
+    // Route::delete('/sizes/{id}',[SizeController::class,'destroy']);
 
-    // Route::apiResource('company-raw-material',CompanyRawMaterialsController::class)->except(['show']);
-    Route::get('/company-raw-material-get',[CompanyRawMaterialsController::class,'index']);
-    Route::post('/company-raw-material',[CompanyRawMaterialsController::class,'store']);
-    Route::put('/company-raw-material/{id}',[CompanyRawMaterialsController::class,'update']);
-    Route::delete('/company-raw-material/{id}',[CompanyRawMaterialsController::class,'destroy']);
+    Route::apiResource('company-raw-material',CompanyRawMaterialsController::class)->except(['show']);
+    // Route::get('/company-raw-material-get',[CompanyRawMaterialsController::class,'index']);
+    // Route::post('/company-raw-material',[CompanyRawMaterialsController::class,'store']);
+    // Route::put('/company-raw-material/{id}',[CompanyRawMaterialsController::class,'update']);
+    // Route::delete('/company-raw-material/{id}',[CompanyRawMaterialsController::class,'destroy']);
 
     Route::apiResource('finish-goods',FinishGoodsController::class)->except(['show']);
-    Route::get('/finish-goods-get',[FinishGoodsController::class,'index']);
-    Route::post('/finish-goods',[FinishGoodsController::class,'store']);
-    Route::put('/finish-goods/{id}',[FinishGoodsController::class,'update']);
-    Route::delete('/finish-goods/{id}',[FinishGoodsController::class,'destroy']);
+    // Route::get('/finish-goods-get',[FinishGoodsController::class,'index']);
+    // Route::post('/finish-goods',[FinishGoodsController::class,'store']);
+    // Route::put('/finish-goods/{id}',[FinishGoodsController::class,'update']);
+    // Route::delete('/finish-goods/{id}',[FinishGoodsController::class,'destroy']);
 
     Route::get('/company-raw-material-calculation',[CompanyRawMaterialsController::class,'calculation'])->name('company-raw-material.calculation');
     Route::get('/finish-goods-calculation',[CompanyRawMaterialsController::class,'calculation'])->name('finish-goods.calculation');
