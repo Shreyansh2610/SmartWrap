91ce3181975a303a297bcd4a55547cb7f3a21e6f
Shreyansh2610 <shreyansh7874056628@gmail.com>
2024-08-04 23:31:22 +0530

    table updated

--- database/migrations/2024_08_04_230049_user_column.php
+++ database/migrations/2024_08_04_230049_user_column.php
@@ -14,8 +14,10 @@ return new class extends Migration
         $tables = ['banks','company_profiles','company_raw_materials','finish_goods','products','sizes'];
         foreach ($tables as $key => $dbTable) {
             Schema::table($dbTable, function (Blueprint $table) {
-                $table->uuid('created_by')->nullable();
+                $table->uuid('created_by')->nullable()->after('id');
             });
+
+            // DB::table($dbTable)->update(['created_by'=>'1e194a12-0ed9-470d-b01e-0275840181a8']);
         }
     }
 
